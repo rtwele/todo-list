8cb042d6ab7b5540c19b3318dd2cb3ff38d0d58b
rtwele <ryantwele@gmail.com>
2020-11-08 09:54:01 -0500

    react app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Take a shower'},
+      {id: 2, content: 'Walk the dogs'}
+    ]
+  }
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    });
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos  = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    )
+  }
+}
+
+export default App
